b61aae230d62be120db04e517568c3019a60b7ef
Daniele Oliveira <daniele.oliveiras@hotmail.com>
2024-03-21 20:54:32 -0300

    começo

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//adicionar produtos ao carrinho
+//visualizar os itens escolhidos
+//calcular o subtotal de cada produto
+
+function carrinho(){
+    let nomeProduto = prompt("Adicione um produto ao carrinho:");
+    let precoProduto = parseFloat(prompt("Informe o preço do produto"));
+    let quantidadeProduto = parseInt(prompt("Informe a quantidade do produto"));
+    console.log(nomeProduto);
+    console.log("R$ " + precoProduto);
+    console.log(quantidadeProduto);
+}
+
+carrinho ();
