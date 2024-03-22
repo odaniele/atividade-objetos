9d5b20ad5b2b95246bfac4d4c5d5c3450c0a260e
Bruna Rodrigues <brunaaheloisa07@gmail.com>
2024-03-22 11:54:45 -0300

    adicionando algumas funcionalidades

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 //visualizar os itens escolhidos
 //calcular o subtotal de cada produto
 
+/*
+
 function carrinho(){
     let nomeProduto = prompt("Adicione um produto ao carrinho:");
     let precoProduto = parseFloat(prompt("Informe o preço do produto"));
@@ -12,5 +14,34 @@ function carrinho(){
 }
 
 carrinho ();
+*/
+function carrinho() {
+    let produtos = []; // Array para armazenar os produtos no carrinho
 
-//test
+    while (true) {
+        let nomeProduto = prompt("Adicione um produto ao carrinho (ou digite 'fim' para encerrar):");
+        if (!nomeProduto) {
+            break; // Encerra o loop se o usuário clicar em "Cancelar" ou fechar o prompt
+        }
+        if (nomeProduto.toLowerCase() === "fim") {
+            break; // Encerra o loop se o usuário digitar 'fim'
+        }
+        let precoProduto = parseFloat(prompt("Informe o preço do produto"));
+        let quantidadeProduto = parseInt(prompt("Informe a quantidade do produto"));
+
+        // Adiciona o produto ao carrinho
+        produtos.push({
+            nome: nomeProduto,
+            preco: precoProduto,
+            quantidade: quantidadeProduto
+        });
+    }
+
+    // Imprime os produtos no carrinho
+    console.log("Produtos no carrinho:");
+    produtos.forEach(produto => {
+        console.log(produto.nome + " - R$ " + produto.preco + " - Quantidade: " + produto.quantidade);
+    });
+}
+
+carrinho();
